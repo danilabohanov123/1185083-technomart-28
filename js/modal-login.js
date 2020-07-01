9472350d5a9500a8feab441f46023d7410622f5a
danilabohanov123 <57310524+danilabohanov123@users.noreply.github.com>
2020-07-01 19:18:56 +0300

    правки3

--- js/modal-login.js
+++ js/modal-login.js
@@ -2,12 +2,8 @@ let sign_in = document.querySelector(".sign-in");
 let sign_up = document.querySelector(".sign-up");
 let modal_login = document.querySelector(".modal-login");
 let modal_close_login = document.querySelector(".modal-close-login");
-let login_form= document.querySelector(".login-form");
-let profile = document.querySelector(".profile");
-let login_login = document.querySelector(".login-login");
-let authorization = document.querySelector(".authorization");
-let personal_account = document.querySelector(".personal-account");
-let cart = document.querySelector(".cart");
+let login_form = document.querySelector(".login-form");
+
 
 sign_in.onclick = function() {
   modal_login.classList.add("modal-show");
@@ -18,11 +14,7 @@ sign_up.onclick = function() {
 };
 
 login_form.onsumbit = function() {
-  profile.textContent = login_login.value;
-  authorization.classList.add('hide');
-  personal_account.classList.add('show-flex');
-  cart.textContent = "Корзина: 10";
-  cart.classList.add('cart-login');
+  modal_login.classList.remove("modal-show");
 };
 
 modal_close_login.onclick = function() {
