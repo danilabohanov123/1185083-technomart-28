9b985ac33c3f168bf62a364aeb0bd5622a9eabe0
danilabohanov123 <57310524+danilabohanov123@users.noreply.github.com>
2020-07-03 15:13:59 +0300

    правки5

--- js/modal-map.js
+++ js/modal-map.js
@@ -1,6 +1,6 @@
 var navigation_map_link = document.querySelector(".navigation-map");
 var modal_map = document.querySelector(".modal-map");
-var modal_close_map = document.querySelector(".modal-close-map");
+var modal_close_map = modal_map.querySelector(".modal-close-map");
 
 navigation_map_link.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -12,10 +12,11 @@ modal_close_map.addEventListener("click", function(evt) {
   modal_map.classList.remove("modal-show");
 });
 
-document.body.addEventListener("keyup", function (e) {
-  var key = e.keyCode;
-
-  if (key == 27) {
-    modal_map.classList.remove("modal-show");
-  };
-}, false);
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal_map.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modal_map.classList.remove("modal-show");
+    }
+  }
+});
--- js/modal-write-us.js
+++ js/modal-write-us.js
@@ -1,21 +1,63 @@
 var write_us_link = document.querySelector(".write-us-link");
 var modal_write_us = document.querySelector(".modal-write-us");
-var modal_close_write_us = document.querySelector(".modal-close-write-us");
+var modal_close_write_us = modal_write_us.querySelector(".modal-close-write-us");
+var write_us_form = modal_write_us.querySelector(".write-us-form");
+var login_email_field = modal_write_us.querySelector(".login-email-field");
+var login_name_field = modal_write_us.querySelector(".login-name-field");
+var message_text_field = modal_write_us.querySelector(".message-text-field");
 
-write_us_link.addEventListener("click", function(evt) {
+var is_storage_support = true;
+var storage_name = "";
+var storage_email = "";
+
+try {
+  storage_name = localStorage.getItem("login-name");
+  storage_email = localStorage.getItem("login-email");
+} catch (err) {
+  is_storage_support = false;
+}
+
+write_us_link.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal_write_us.classList.add("modal-show");
+
+  if (storage_name) {
+    login_name_field.value = storage_name;
+    login_email_field.value = storage_email;
+    message_text_field.focus();
+  } else {
+    login_name_field.focus();
+  }
 });
 
-modal_close_write_us.addEventListener("click", function(evt) {
+modal_close_write_us.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal_write_us.remove("modal-show");
+  modal_write_us.classList.remove("modal-show");
+  modal_write_us.classList.remove("modal-error");
 });
 
-document.body.addEventListener("keyup", function (e) {
-  var key = e.keyCode;
+write_us_form.addEventListener("submit", function (evt) {
+  console.log("sdfsdf");
+  if (!login_email_field.value || !login_name_field.value || !message_text_field.value) {
+    evt.preventDefault();
+    modal_write_us.classList.add("modal-error");
+    modal_write_us.offsetWidth = modal_write_us.offsetWidth;
+    modal_write_us.classList.remove("modal-error");
+  } else {
+    if (is_storage_support){
+      localStorage.setItem("login-name", login_name_field.value);
+      localStorage.setItem("login-email", login_email_field.value);
+      localStorage.setItem("message-text", "");
+    }
+  }
+});
 
-  if (key == 27) {
-    modal_write_us.remove("modal-show");
-  };
-}, false);
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal_write_us.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modal_write_us.classList.remove("modal-show");
+      modal_write_us.classList.remove("modal-error");
+    }
+  }
+});
