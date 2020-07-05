e5d20a62811b02154eb26abec2451c2d61364adb
danilabohanov123 <57310524+danilabohanov123@users.noreply.github.com>
2020-07-05 21:38:10 +0300

    правки

---
+++ js/modal-added-to-cart.js
@@ -0,0 +1,36 @@
+var cartCounter = 0;
+var addToCartElements = document.getElementsByClassNamer('.add-to-cart');
+var cart = document.querySelector('.cart');
+var modalAddedToCart = document.querySelector('.modal-added-to-cart');
+var modalCloseMessage = modalAddedToCart.querySelector('.modal-close-message');
+var closeMessage = modalAddedToCart.querySelector('.close-message');
+
+addToCartCartElements.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  console.log(cartCounter);
+  cartCounter += 1;
+  if (cartCounter == 1) {
+    cart.classList.add('cart-login');
+  }
+  cart.textContent = 'Корзина ' + cartCounter;
+  modalAddedToCart.classList.add('modal-show');
+});
+
+modalCloseMessage.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalAddedToCart.classList.remove('modal-show');
+});
+
+closeMessage.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalAddedToCart.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalAddedToCart.classList.contains('modal-show')) {
+      evt.preventDefault();
+      modalAddedToCart.classList.remove('modal-show');
+    }
+  }
+});
